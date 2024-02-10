ffc31584d954d75e653d795ba01c4d95dc4e5bd3
Necritto <onetojj@gmail.com>
2024-02-10 10:10:34 +0300

    feat: install pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,24 @@ import {VitePWA} from 'vite-plugin-pwa';
 
 const vitePwa = VitePWA({
     registerType: 'autoUpdate',
+    manifest: {
+        scope: '/',
+        start_url: '/',
+        name: 'Tech review checker',
+        short_name: 'TR_Checker',
+        description: 'App for convenient technical review',
+        theme_color: '#ffffff',
+        icons: [{
+            src: 'assets/images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+        },
+        {
+            src: 'assets/images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+        }],
+    },
 });
 
 export default defineConfig({
