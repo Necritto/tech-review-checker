9eb3e6923e1d4a926b5413bf87b0840ea6ed4845
Necritto <onetojj@gmail.com>
2024-02-10 12:53:49 +0300

    feat: create vue app skeleton

---
+++ src/components/AppFooter/component.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer class="footer">
+        AppFooter
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@import './styles';
+</style>
