335920b2d0f877730510103e1a815959e76d288d
Necritto <onetojj@gmail.com>
2024-02-10 09:21:23 +0300

    feat: init app

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix', 'refactor', 'style', 'test']],
+        'header-max-length': [2, 'always', 150],
+    },
+};
