8b68dc2679839c69127551b9871b801c50c53af3
Necritto <onetojj@gmail.com>
2024-02-11 19:23:02 +0300

    feat: header menu

--- src/components/AppHeader/component.vue
+++ src/components/AppHeader/component.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+
 import LogoSvg from '@/assets/images/icons/check.svg?component';
+import ExpandSvg from '@/assets/images/icons/expand.svg?component';
 import UIButton from '@components/UI/UIButton/component.vue';
 import LoadFilesButton from '@components/LoadFilesButton/component.vue';
+import Collapse from '@components/UI/Collapse/component.vue';
 import {jsonToObject} from '@utils/normalizer';
 import {useQuestionsStore} from '@stores/questions';
 import type {Questions} from '@stores/questions/types';
 
 const questionsStore = useQuestionsStore();
+const isPanelOpen = ref<boolean>(false);
 
 async function handleLoadQuestions(file: File) {
     const result = await jsonToObject(file) as Questions[];
@@ -16,24 +21,41 @@ async function handleLoadQuestions(file: File) {
 function handleDelete() {
     questionsStore.setQuestions([]);
 }
+
+function handleTogglePanel() {
+    isPanelOpen.value = !isPanelOpen.value;
+}
 </script>
 
 <template>
     <header class="container app-header">
         <LogoSvg class="app-logo" />
-        <section class="app-header-actions">
-            <LoadFilesButton
-                class="action-button"
-                label="Загрузить вопросы"
-                accept="application/json"
-                @load="((files: FileList) => handleLoadQuestions(files[0]))"
-            />
-            <UIButton
-                class="action-button"
-                label="Удалить вопросы"
-                @click="handleDelete"
-            />
-        </section>
+        <UIButton
+            :class="['panel-button', {'panel-button--open': isPanelOpen}]"
+            @click="handleTogglePanel"
+        >
+            <span>Возможности</span>
+            <ExpandSvg />
+        </UIButton>
+        <Collapse
+            :open="isPanelOpen"
+            class="app-header-expand"
+        >
+            <hr />
+            <section class="app-header-actions">
+                <LoadFilesButton
+                    class="action-button"
+                    label="Загрузить вопросы"
+                    accept="application/json"
+                    @load="((files: FileList) => handleLoadQuestions(files[0]))"
+                />
+                <UIButton
+                    class="action-button"
+                    label="Удалить вопросы"
+                    @click="handleDelete"
+                />
+            </section>
+        </Collapse>
     </header>
 </template>
 
